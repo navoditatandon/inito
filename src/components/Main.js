965ff79ca3c0da0b1e325dd46aefd65e7525e575
navodita.tandon <navodita.tandon@myntra.com>
2024-07-06 15:08:56 +0530

    page 6

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,7 @@ import Page2 from "./Page2";
 import Page3 from "./Page3";
 import Page4 from "./Page4";
 import Page5 from "./Page5";
+import Page6 from "./Page6";
 
 const Main = () => {
     return(
@@ -13,6 +14,7 @@ const Main = () => {
         <Page3 />
         <Page4 />
         <Page5 />
+        <Page6 />
         </>
     )
 }
---
+++ src/components/content5.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Page6.css'; 
+
+const Content4 = () => {
+    return (
+        <div style={{display: "flex", justifyContent: "center", flexDirection: "column", alignItems : "center", padding : "90px 0 20px 0"}}>
+           <h1>Adapts to your irregular cycle</h1>
+           <p style = {{color: "black"}}>Most ovulation kits have a set value or a threshold, making them likely to give a false read if your hormones fall outside the average range.</p><br/>
+           <p style = {{color: "black"}}>Every woman is unique, especially with a condition like PCOS or Thyroid which can influence hormone levels. Inito measures real values of your hormones to personalize results to the unique you.</p>
+        </div>
+    )
+}
+
+export default Content4;
